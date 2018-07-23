99e0192753ff51a8d88f0b8d7d4f64a10e980093
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-23 13:40:33 -0400

    test if environment checkers are working for the two environment setups

--- monday.sh
+++ monday.sh
@@ -12,11 +12,28 @@ git add foo.txt
 iDoStuff() {
 echo 'do stuff function initiated'
 
-for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep 'staged'; done;
+## if git status has files being tracked
+output1=$(for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep 'staged'; done;)
 
 ## if git status is completely clean, this will return nothing!
 # just dont have a branch with '(' in the name.
-for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep '('; done;
+output2=$(for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep '('; done;)
+if [ -z "$output1" ];
+then
+      echo "1 is empty"
+      echo "no files are being tracked lets check untracked next"
+else
+      echo "1 is NOT empty"
+      echo "you have tracked files that need to be addressed! 😱"
+fi
+if [ -z "$output2" ];
+then
+      echo "2 is empty"
+      echo "git directory is completely clean, good to go!"
+else
+      echo "2 is NOT empty"
+      echo "you have untracked or tracked files that need to be addressed! 😱 "
+fi
 }
 cleanEnvironment(){
 echo 'clean environment initiated'
