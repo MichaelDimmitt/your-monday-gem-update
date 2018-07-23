6eea85f166c179ec608bf0cad308e17bf10f7d33
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-23 12:57:53 -0400

    set up environment to test bash script

--- monday.sh
+++ monday.sh
@@ -1,6 +1,30 @@
 ## if git status has nothing tracked, this will return nothing!
-for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep 'staged'; done
+# set up the environment
+setEnvironment() {
+echo 'set environment initiated'
+touch test.txt # untracked
+touch blah.txt # tracked but not staged
+touch foo.txt  # tracked and staged
+git add -N blah.txt
+git add foo.txt
+}
+
+iDoStuff() {
+echo 'do stuff function initiated'
+
+for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep 'staged'; done;
 
 ## if git status is completely clean, this will return nothing!
-# just dont have a branch with '(' in the name. 
-for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep '('; done
+# just dont have a branch with '(' in the name.
+for BRANCH in `git branch --list| cut -c 3-`; do git checkout $BRANCH -q; git status | grep '('; done;
+}
+cleanEnvironment(){
+echo 'clean environment initiated'
+git reset HEAD foo.txt >&/dev/null && git rm --cached blah.txt >&/dev/null
+rm blah.txt foo.txt test.txt >&/dev/null
+}
+
+setEnvironment
+iDoStuff
+cleanEnvironment
+iDoStuff
