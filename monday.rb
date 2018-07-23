e6ffedcbfc621f58b7a8c20b64b69c20fd94ba13
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-23 15:38:51 -0400

    bundler was needed for project

--- monday.rb
+++ monday.rb
@@ -1,5 +1,11 @@
 # never let user input go into a system command
 system(". monday.sh")
+## the below command needs to change in the future
+## probably get the name of the first branch and then pass it in here.
+## or always create a new branch called monday-update
+system("git checkout start-interacting-with-gems")
+system("gem install bundler")
+system("bundle install")
 require_relative 'gemfile_updater.rb'
 
 gem_manager = GemfileUpdater.new
